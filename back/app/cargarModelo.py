5f0099733bbf0df40f4af3eedc93c173978feee3
Daneil Torres <danielfelipetorreslopez@gmail.com>
2025-06-04 21:51:13 -0500

    codigo modificado para correrse con un solo comando

--- back/app/cargarModelo.py
+++ back/app/cargarModelo.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-def cargarModelo(peso_path="best1.pt"):
+def cargarModelo(peso_path="best12.pt"):
     print("Cargando modelo YOLO...")
     model = YOLO(peso_path)
     print("Modelo YOLO cargado")
---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+start cmd /k "cd back && python run.py"
+start cmd /k "cd front && npm run dev" 
